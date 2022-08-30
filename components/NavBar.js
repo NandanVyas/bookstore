b9c12a0d9106c31b30360745ac0c2ccf4501f5ec
NandanVyas <57463564+NandanVyas@users.noreply.github.com>
2022-08-30 16:29:01 +0530

    Changed NavBar Made Seperate Component for Cart

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,24 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {AiOutlineShoppingCart} from 'react-icons/Ai'
+import SideBar from './SideBar'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between flex-col items-center md:flex-row md:justify-start py-2'>
+    <div className='flex justify-between flex-col items-center md:flex-row md:justify-start py-1 shadow-md'>
         <div className='logo mx-5'>
-            <Image src='/N V Bookstore-1.png' alt='' height={45} width={200} />
+            <Link href='/'><a><Image src='/N V Bookstore-1.png' alt='' height={45} width={200} /></a></Link>
         </div>
         <div className='nav'>
-            <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-                <Link href='/'><a><li>Books</li></a></Link>
+            <ul className='flex items-center space-x-4 font-bold md:text-base'>
+                <Link href='/books'><a><li>Books</li></a></Link>
                 <Link href='/'><a><li>Menu2</li></a></Link>
                 <Link href='/'><a><li>Menu3</li></a></Link>
                 <Link href='/'><a><li>Menu4</li></a></Link>
             </ul>
         </div>
-        <div className='cart absolute right-0 top-2 mx-4'>
-            <AiOutlineShoppingCart className='text-xl md:text-3xl'/>
-        </div>
+        
+        <SideBar/>
     </div>
   )
 }
