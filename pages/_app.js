12b65521e2ff6c1bd4ff3d6e6b9487a49d1a75dc
NandanVyas <57463564+NandanVyas@users.noreply.github.com>
2022-08-30 16:29:34 +0530

    Few Changes Regarging Cart

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><NavBar/><Component {...pageProps} /><Footer/></>
 }
 
 export default MyApp
