76a14182f7de0b746850953f8078016c52443d3b
Nandan Vyas <vyasnandu123@gmail.com>
2022-08-30 21:34:42 +0530

    Updated!!!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import '../styles/globals.css'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+import "../styles/globals.css";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <><NavBar/><Component {...pageProps} /><Footer/></>
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
