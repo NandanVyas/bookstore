6cfbeb22df00c3c175d703ec924abac0e51dfc70
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-05 17:31:52 +0530

    created checkout page

---
+++ pages/checkout.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Delivery from "../components/Delivery";
+import Review from '../components/Review';
+import Link from "next/link";
+
+import {
+  AiOutlineCloseCircle,
+  AiOutlineShoppingCart,
+  AiOutlineMinusCircle,
+  AiOutlinePlusCircle,
+} from "react-icons/Ai";
+import { BsFillCartCheckFill } from "react-icons/Bs";
+
+const checkout = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
+  return (
+  <div className='container px-2 sm:m-auto'>
+    <div className=' text-3xl text-center font-bold my-8 '>Checkout</div>
+    <Delivery />
+    <Review cart={cart} addToCart={addToCart} removeFromCart ={removeFromCart} subTotal={subTotal}/>
+    
+    
+    <Link href={''}><button className="flex mr-4  text-white bg-orange-500 border-0 py-2 px-2 focus:outline-none hover:bg-orange-600 rounded text-lg">
+            {" "}
+            <BsFillCartCheckFill className="m-1" /> Pay ₹:{subTotal}
+          </button></Link>
+    </div>
+  )
+}
+
+export default checkout
