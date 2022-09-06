9aa40ea341e4b0a7aba8da254726cce1642ca151
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-06 17:26:38 +0530

    added mongoose for mongoDB

---
+++ middleware/mongoose.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB=handler=>async (req,res)=>{
+    if(mongoose.connections[0].readyState){
+        return handler(req,res)
+    }
+    await mongoose.connect(process.env.MONGO_URI)
+    return handler(req,res);
+}
+
+export default connectDB;
