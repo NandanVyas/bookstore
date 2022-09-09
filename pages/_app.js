c8c90ba2145b656870180ccd51be852971826b93
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-09 21:41:29 +0530

    added logout function

--- pages/_app.js
+++ pages/_app.js
@@ -1,95 +1,113 @@
 import "../styles/globals.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Router, useRouter } from "next/router";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
+  const [cart, setCart] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+  const [user, setUser] = useState({ value: null });
+  const [key, setKey] = useState(0);
+  const router = useRouter();
 
-  
-const [cart, setCart] = useState({})
-const [subTotal, setSubTotal] = useState(0)
-const [user, setUser] = useState({value: null})
-const [key, setKey] = useState(0)
-const router=useRouter()
+  useEffect(() => {
+    console.log("hey i am nandan useeffect from app.js");
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")));
+        saveCart(JSON.parse(localStorage.getItem("cart")));
+      }
+    } catch (error) {
+      console.error(error);
+      localStorage.clear();
+    }
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser({ value: token });
+      setKey(Math.random());
+    }
+  }, [router.query]);
 
-useEffect(() => {
-  console.log("hey i am nandan useeffect fromj app.js")
-  try
-  {
-    if(localStorage.getItem("cart"))
-  {
-    setCart(JSON.parse(localStorage.getItem("cart")))
-    saveCart(JSON.parse(localStorage.getItem("cart")))
-  }
-}
-  catch(error){
-    console.error(error)
-    localStorage.clear()
-    
-  }
-  const token =localStorage.getItem('token')
-  if(token)
-  {
-    setUser({value:token})
-    setKey(Math.random())
-  }
-  
-}, [router.query])
+  const saveCart = (myCart) => {
+    localStorage.setItem("cart", JSON.stringify(myCart));
+    let subt = 0;
+    let keys = Object.keys(myCart);
+    for (let i = 0; i < keys.length; i++) {
+      //console.log(keys)
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;
+    }
+    setSubTotal(subt);
+  };
 
+  const logout = () => {
+    localStorage.removeItem("token");
+    toast.success("You have been logged out", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    setTimeout(() => {
+      setUser({ value: null });
+      setKey(Math.random());
+      router.push("http://localhost:3000/login");
+    }, 3000);
+  };
+  const addToCart = (itemCode, qty, price, name, author) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = newCart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] = { qty: 1, price, name, author };
+    }
+    setCart(newCart);
+    //console.log(newCart)
+    saveCart(newCart);
+  };
 
-const saveCart=(myCart)=>{
-  localStorage.setItem("cart",JSON.stringify(myCart))
-  let subt=0
-  let keys=Object.keys(myCart)
-  for(let i=0; i<keys.length;i++)
-  {
-    //console.log(keys)
-    subt+=myCart[keys[i]].price * myCart[keys[i]].qty;
-  }
-  setSubTotal(subt)
-}
+  const removeFromCart = (itemCode, qty, price, name, author) => {
+    let newCart = JSON.parse(JSON.stringify(cart));
+    if (itemCode in cart) {
+      newCart[itemCode].qty = newCart[itemCode].qty - qty;
+    }
+    if (newCart[itemCode].qty <= 0) {
+      delete newCart[itemCode];
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
 
-const addToCart= (itemCode,qty,price,name,author) => {
-  let newCart=cart
-  if(itemCode in cart){
-    newCart[itemCode].qty=newCart[itemCode].qty + qty;
-  }
-  else{
-    newCart[itemCode]={qty:1,price,name,author}
-  }
-  setCart(newCart)
-  //console.log(newCart)
-  saveCart(newCart)
-
-}
-
-const removeFromCart= (itemCode,qty,price,name,author) => {
-  let newCart=JSON.parse(JSON.stringify(cart))
-  if(itemCode in cart){
-    newCart[itemCode].qty=newCart[itemCode].qty - qty;
-  }
-  if(newCart[itemCode].qty<=0)
-  {
-    delete newCart[itemCode]
-  }
-  setCart(newCart)
-  saveCart(newCart)
-
-}
-
-
-const clearCart =()=>{
-  setCart({})
-  saveCart({})
-}
-
+  const clearCart = () => {
+    setCart({});
+    saveCart({});
+  };
 
   return (
     <>
-      <NavBar user={user} key={key} cart={cart} addToCart={addToCart}  removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
-      <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
+      <NavBar
+        logout={logout}
+        user={user}
+        key={key}
+        cart={cart}
+        addToCart={addToCart}
+        removeFromCart={removeFromCart}
+        clearCart={clearCart}
+        subTotal={subTotal}
+      />
+      <Component
+        cart={cart}
+        addToCart={addToCart}
+        removeFromCart={removeFromCart}
+        clearCart={clearCart}
+        subTotal={subTotal}
+        {...pageProps}
+      />
       <Footer />
     </>
   );
