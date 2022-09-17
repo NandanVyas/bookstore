9ccc28deb1f78e72745548cccc7040e7439b6b46
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-17 19:20:20 +0530

    password change feature added

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Delivery from "../components/Delivery";
-import Review from "../components/Review";
-import Link from "next/link";
-import Head from "next/head";
-import Script from "next/script";
-import { BsFillCartCheckFill } from "react-icons/Bs";
 import { useState, useEffect } from "react";
-import { Router } from "next/router";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const jwt = require("jsonwebtoken");
+import { useRouter } from "next/router";
 
 const Profile = () => {
+  const router = useRouter();
   // const jwt=require("jsonwebtoken");
   const [token, setToken] = useState("");
   const [address, setAddress] = useState("");
@@ -20,6 +18,21 @@ const Profile = () => {
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [password, setPassword] = useState("");
+  const [newpassword, setNewpassword] = useState("");
+  const [confirmnewpassword, setConfirmnewpassword] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.name == "password") {
+      setPassword(e.target.value);
+    }
+    else if (e.target.name == "newpassword") {
+      setNewpassword(e.target.value);
+    }
+     else if (e.target.name == "confirmnewpassword") {
+      setConfirmnewpassword(e.target.value);
+    }
+  };
 
   useEffect(() => {
     const mytoken = localStorage.getItem("token");
@@ -28,16 +41,10 @@ const Profile = () => {
     } else {
       // console.log(mytoken);
       setToken(mytoken);
-
-      const data = jwt.verify(mytoken, "secretjwt");
-      // console.log(data);
-
-      setEmail(data.email);
-      setName(data.name);
       fetchData(mytoken);
     }
-  }, []);
-  
+  }, [router]);
+
   const fetchData = async (mytoken) => {
     // const token = localStorage.getItem("token");
     let data = { token: mytoken };
@@ -53,18 +60,26 @@ const Profile = () => {
     // console.log(res);
     // console.log("yay")
     // console.log("Address is ",res.address)
+    setEmail(res.user.email);
+    setName(res.user.name);
     setAddress(res.user.address);
     setCity(res.user.city);
     setPhone(res.user.phone);
-    setPincode(res.user.pincode)
-    setState(res.user.state)
-    
-
+    setPincode(res.user.pincode);
+    setState(res.user.state);
   };
 
   const handleUpdateDetails = async () => {
     // const token = localStorage.getItem("token");
-    let data = { token:token,address:address,name:name ,pincode:pincode,state:state,city:city,phone:phone};
+    let data = {
+      token: token,
+      address: address,
+      name: name,
+      pincode: pincode,
+      state: state,
+      city: city,
+      phone: phone,
+    };
     let a = await fetch(
       `${process.env.NEXT_PUBLIC_HOST}/api/updateUserDetails`,
       {
@@ -76,11 +91,72 @@ const Profile = () => {
       }
     );
     let res = await a.json();
+    toast.success("Profile Updated !!", {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    // console.log(res);
+  };
+
+  const handleUpdatePassword = async () => {
+    // const token = localStorage.getItem("token");
+    let data = {
+      token: token,
+      password: password,
+      newpassword: newpassword,
+      confirmnewpassword: confirmnewpassword,
+    };
+    let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/updatePassword`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let res = await a.json();
+    if (res.success) {
+      toast.success("Password Updated !!", {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      toast.error(res.error, {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+
     // console.log(res);
   };
 
   return (
     <div className="dark:bg-gray-900 min-h-[60vh] px-20">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className=" text-3xl text-center font-bold py-8 dark:text-orange-300 ">
         Profile{" "}
       </div>
@@ -101,7 +177,7 @@ const Profile = () => {
         state={state}
         setState={setState}
       />
-      
+
       <button
         onClick={handleUpdateDetails}
         className="disabled:bg-orange-200 flex mr-4 mt-5 text-white bg-orange-500 border-0 py-2 px-2 focus:outline-none hover:bg-orange-600 rounded text-sm"
@@ -109,11 +185,69 @@ const Profile = () => {
         Update Details
       </button>
 
-      <div className="text-xl font-semibold my-4">
+      <div className="text-xl font-semibold my-6 dark:text-orange-300">
         2. Change Password
       </div>
-
-
+      <div className="w-1/2 mb-4">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-orange-50"
+        >
+          Password
+        </label>
+        <input
+          value={password}
+          onChange={handleChange}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="••••••••"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-300 dark:placeholder-gray-400 dark:text-orange-200 dark:focus:ring-orange-500 dark:focus:border-orange-500"
+          required=""
+        />
+      </div>
+      <div className="w-1/2 mb-4">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-orange-50"
+        >
+          New Password
+        </label>
+        <input
+          value={newpassword}
+          onChange={handleChange}
+          type="password"
+          name="newpassword"
+          id="newpassword"
+          placeholder="••••••••"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-300 dark:placeholder-gray-400 dark:text-orange-200 dark:focus:ring-orange-500 dark:focus:border-orange-500"
+          required=""
+        />
+      </div>
+      <div className="w-1/2 mb-6">
+        <label
+          htmlFor="confirmpassword"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-orange-50"
+        >
+          Confirm New password
+        </label>
+        <input
+          value={confirmnewpassword}
+          onChange={handleChange}
+          type="password"
+          name="confirmnewpassword"
+          id="confirmnewpassword"
+          placeholder="••••••••"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-300 dark:placeholder-gray-400 dark:text-orange-200 dark:focus:ring-orange-500 dark:focus:border-orange-500"
+          required=""
+        />
+      </div>
+      <button
+        onClick={handleUpdatePassword}
+        className="disabled:bg-orange-200 flex mr-4 mt-5 text-white bg-orange-500 border-0 py-2 px-2 focus:outline-none hover:bg-orange-600 rounded text-sm"
+      >
+        Update Password
+      </button>
     </div>
   );
 };
