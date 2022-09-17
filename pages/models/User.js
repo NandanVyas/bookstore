2fc08098248d41cfdb9d51afb25692dc78d9e1be
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-17 14:19:20 +0530

    added address to user model

--- pages/models/User.js
+++ pages/models/User.js
@@ -1,15 +1,21 @@
 // Starting mongoDB
 // import mongoose from 'mongoose';
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    name: { type : String , required : true},
-    email: { type : String , required : true ,unique:true },
-    password: { type : String , required : true},
-    
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: String, default: "" },
+    pincode: { type: String, default: "" },
+    phone: { type: String, default: "" },
+    city: { type: String, default: "" },
+    state: { type: String, default: "" },
   },
-  {timestamps:true});
+  { timestamps: true }
+);
 
-  mongoose.models = {}
-  export default mongoose.model("User",UserSchema) ;
+mongoose.models = {};
+export default mongoose.model("User", UserSchema);
