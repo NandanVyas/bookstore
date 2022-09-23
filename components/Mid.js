92f3975f69ffdd5f4d533c07fbf2bab3301871cf
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-23 22:26:27 +0530

    minor bug fix

--- components/Mid.js
+++ components/Mid.js
@@ -80,7 +80,6 @@ const Mid = () => {
         </div>
       </div>
     </div>
-    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" control-id="ControlID-86">Button</button>
   </div>
 </section>
     </div>
