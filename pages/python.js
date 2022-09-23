c87066e9fb5ef4b5260f4513fa0ee10d10532b11
Nandan Vyas <vyasnandu123@gmail.com>
2022-09-23 20:30:49 +0530

    pages created for specific categories

--- pages/python.js
+++ pages/python.js
@@ -3,18 +3,19 @@ import React from "react";
 import Link from "next/link";
 import Book from "./models/Book";
 import mongoose from "mongoose";
+import { capitalize } from "@mui/material";
 
-const Books = ({ books }) => {
+const Python = ({ books }) => {
   //console.log(books);
   return (
     <div>
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font dark:bg-gray-900">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4 justify-center">
+          <div className="flex flex-wrap -m-4 justify-center space-y-4">
             {books.map((item) => {
               return (
                 <Link passHref={true} key={item._id} href={`/product/${item.slug}`}>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg ">
+                  <div className="lg:w-1/4 md:w-1/2 p-10 w-full cursor-pointer shadow-lg hover:scale-125 hover:bg-white dark:hover:bg-gray-700 dark:bg-gray-900 dark:shadow-orange-600 duration-500">
                     <a className="block relative rounded overflow-hidden">
                       <img
                         alt="ecommerce"
@@ -23,16 +24,16 @@ const Books = ({ books }) => {
                       />
                     </a>
                     <div className="mt-4 text-center md:text-left">
-                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                        {item.year}
+                      <h3 className="text-gray-500 dark:text-gray-200 text-xs tracking-widest title-font mb-1">
+                        {capitalize(item.category) }
                       </h3>
-                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                      <h2 className="text-gray-900 dark:text-white title-font text-lg font-medium">
                         {item.title}
                       </h2>
-                      <h2 className="text-gray-700 title-font text-base font-medium">
+                      <h2 className="text-gray-700 dark:text-white title-font text-base font-medium">
                         {item.author}
                       </h2>
-                      <p className="mt-1">₹{item.price}</p>
+                      <p className="mt-1 dark:text-white">₹{item.price}</p>
                     </div>
                   </div>
                 </Link>
@@ -48,10 +49,10 @@ export async function getServerSideProps(context) {
   if (!mongoose.connections[0].readyState) {
     await mongoose.connect(process.env.MONGO_URI);
   }
-  let books = await Book.find();
+  let books = await Book.find({category:"python"});
   return {
     props: { books: JSON.parse(JSON.stringify(books)) }, // will be passed to the page component as props
   };
 }
 
-export default Books;
+export default Python;
