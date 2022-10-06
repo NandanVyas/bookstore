043efdbb4c5a4ed382ea67b6b2812f82ca397036
Nandan Vyas <vyasnandu123@gmail.com>
2022-10-06 22:10:39 +0530

    Create return policy page

---
+++ pages/returnpolicy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Returnpolicy = () => {
+  return (<>
+    <div className='text-3xl text-center font-bold py-8 dark:text-orange-300 '>Returnpolicy</div>
+    <div className='text-2xl text-center font-bold py-8 text-gray-600 dark:text-gray-300 '> No returns allowed yet !! </div>
+    </>
+  )
+}
+
+export default Returnpolicy
