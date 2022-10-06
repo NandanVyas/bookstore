b87a196e2822a93b0f9f8a4e2b40c038ab06a05f
Nandan Vyas <vyasnandu123@gmail.com>
2022-10-06 22:10:21 +0530

    Create Privacy Policy page

---
+++ pages/privacy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div className='text-3xl text-center font-bold py-8 dark:text-orange-300 '>Privacy Policy</div>
+  )
+}
+
+export default Privacy  
