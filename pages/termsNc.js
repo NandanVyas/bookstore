2b712ee83f6ee5b88a858b0f8ae6cdd8a5cce8a7
Nandan Vyas <vyasnandu123@gmail.com>
2022-10-06 22:09:42 +0530

    Create Terms and Conditions page

---
+++ pages/termsNc.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TermsNc = () => {
+  return (
+   <div className='text-3xl text-center font-bold py-8 dark:text-orange-300 '>Terms and Conditions</div> 
+  )
+}
+
+export default TermsNc
